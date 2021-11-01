0a6b6f0a41bb8065650c3b29ebf3525e3c109e7d
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-11-01 20:37:59 +0200

    custom datalist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -144,13 +144,13 @@
                             @csrf
                             <input class="sell_order_id" type="hidden" name="order_id"
                                    value="{{$first_order?$first_order[0]->order_number:'none'}}">
-                            <input type="text" class="form-control" name="customer_name" placeholder="اسم العميل"
-                                   list="customers">
-                            <datalist id="customers">
+                            <input type="hidden" class="form-control" id="customer_name" name="customer_name" placeholder="اسم العميل" value=""
+                                   {{--list="customers"--}}/>
+                            <select id="customerslist" class="form-control" placeholder="اسم العميل">
                                 @foreach ($customers as $customer)
-                                    <option value="{{$customer->name}}">
+                                    <option value="{{$customer->name}}">{{$customer->name}}</option>
                                 @endforeach
-                            </datalist>
+                            </select>
                             <br>
                             <center>
                                 <button type="submit" class="btn btn-success">تأكيد</button>
@@ -199,5 +199,7 @@
                 },
             });
         });
+
+        $('#customerslist').dataList({return_mask:'text', value_selected_to:'customer_name', clearOnFocus: true});
     </script>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <script src="{{ asset('js/app.js') }}"></script>
     {{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--}}
     <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('datalist/dataList.js')}}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -23,6 +24,7 @@
     <link href="{{asset('img/logo.jpg')}}" rel="shortcut icon"/>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('datalist/dataList.css')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('style')
 </head>
