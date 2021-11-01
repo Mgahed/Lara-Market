662c0bd27ea92f206c5aeea725fa5ea6d50da91d
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-11-01 16:17:40 +0200

    update datatable json local arabic

--- public/plugins/datatables/datatables-demo.js
+++ public/plugins/datatables/datatables-demo.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('#dataTable').DataTable({
         "language": {
             // "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Arabic.json"
-            // "url": "http://127.0.0.1:8000/plugins/datatables/arabic.json"
-            "url": "http://localhost:8000/plugins/datatables/arabic.json"
+            "url": "http://127.0.0.1:8000/plugins/datatables/arabic.json"
+            // "url": "http://localhost:8000/plugins/datatables/arabic.json"
         }
     });
 });
